ca8f4837a983ec8f1c18cd38569bab8d75989ece
#V0ID <hey@v0id.me>
2025-02-03 00:17:21 +0300

    refactor: Simplify URL imports by using wildcard import from views

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('api/menu-items', views.MenuItemsView.as_view()),
-    path('api/menu-items/<int:pk>', views.MenuItemView.as_view()),
-    path('api/categories', views.CategoryView.as_view()),
-    path('api/groups/manager/users', views.ManagerUsersView.as_view()),
-    path('api/groups/manager/users/<int:userId>', views.ManagerUserView.as_view()),
-    path('api/groups/delivery-crew/users', views.DeliveryCrewUsersView.as_view()),
-    path('api/groups/delivery-crew/users/<int:userId>', views.DeliveryCrewUserView.as_view()),
-    path('api/cart/menu-items', views.CartView.as_view()),
-    path('api/orders', views.OrdersView.as_view()),
-    path('api/orders/<int:orderId>', views.OrderView.as_view()),
+    path('api/menu-items', MenuItemsView.as_view()),
+    path('api/menu-items/<int:pk>', MenuItemView.as_view()),
+    path('api/categories', CategoryView.as_view()),
+    path('api/groups/manager/users', ManagerUsersView.as_view()),
+    path('api/groups/manager/users/<int:userId>', ManagerUserView.as_view()),
+    path('api/groups/delivery-crew/users', DeliveryCrewUsersView.as_view()),
+    path('api/groups/delivery-crew/users/<int:userId>', DeliveryCrewUserView.as_view()),
+    path('api/cart/menu-items', CartView.as_view()),
+    path('api/orders', OrdersView.as_view()),
+    path('api/orders/<int:orderId>', OrderView.as_view()),
 ]
